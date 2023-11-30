412cb90be5b6b3a787ff92c492af2f3219e8f423
zaid <zaid97abyi@gmail.com>
2023-11-30 11:11:19 +0300

    Add user domain

---
+++ Bakasov.Core/Entities/Users/User.cs
@@ -0,0 +1,14 @@
+﻿namespace Bakasov.Core.Entities.Users;
+
+public class User
+{
+    public int Id { get; set; }
+
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public string Email { get; set; }
+
+    public string PhoneNumber { get; set; }
+}
