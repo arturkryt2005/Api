c571e43de3acbb5649e4f84cbc0714878a083ffa
Артур <@DESKTOP-0ISUU4Q>
2023-12-15 14:59:46 +0300

    AddUserController

---
+++ Bakasov.Api/Controllers/UserController.cs
@@ -0,0 +1,85 @@
+﻿using Bakasov.Core.Entities.Users;
+using Bakasov.Core.Repositories.Users;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Bakasov.Api.Controllers
+{
+    /// <summary>
+    /// Class UserController.
+    /// Implements the <see cref="ControllerBase" />
+    /// </summary>
+    /// <seealso cref="ControllerBase" />
+    [ApiController]
+    [Route("api/[controller]")]
+
+    public class UserController : ControllerBase
+    {
+        /// <summary>
+        /// The user repository
+        /// </summary>
+        private readonly IUserRepository _userRepository;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="UserController" /> class.
+        /// </summary>
+        /// <param name="userRepository">The user repository.</param>
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        /// <summary>
+        /// Gets the users.
+        /// </summary>
+        /// <returns>List&lt;User&gt;.</returns>
+        [HttpGet]
+        public async Task<List<User>> GetUsers()
+        {
+            return await _userRepository.GetListAsync();
+        }
+
+        /// <summary>
+        /// Adds the user.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <returns>IActionResult.</returns>
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] User user)
+        {
+            try
+            {
+                await _userRepository.AddAsync(user);
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e);
+            }
+        }
+
+        /// <summary>
+        /// Deletes the user.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>IActionResult.</returns>
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            var result = await _userRepository.DeleteAsync(id);
+            return Ok(result ? $"Пользователь с id = {id} удален." : $"Удаляемый пользователь с id = {id} не найден.");
+        }
+
+        /// <summary>
+        /// Updates the user.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <returns>IActionResult.</returns>
+        [HttpPut]
+        public async Task<IActionResult> Updateuser([FromBody] User user)
+        {
+            var result = await _userRepository.UpdateAsync(user);
+            return Ok(result ? $"Пользователь с id = {user.Id} обновлен." : $"Пользователь с id = {user.Id} не найден.");
+        }
+    }
+}
